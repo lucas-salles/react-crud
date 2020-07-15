47a9cdcdd588246571e0f3d573a25e9a70cc3b69
Lucas Sales <lucassales398@gmail.com>
2020-07-15 07:07:17 -0300

    componente header

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -1,4 +1,11 @@
 import "./Header.css";
 import React from "react";
 
-export default (props) => <header className="header">Header</header>;
+export default (props) => (
+  <header className="header d-none d-sm-flex flex-column">
+    <h1>
+      <i className={`fa fa-${props.icon}`}></i> {props.title}
+    </h1>
+    <p className="lead text-muted">{props.subtitle}</p>
+  </header>
+);
