27660e383b7d16c94083a22b09c38b3a7a105a60
Lucas Sales <lucassales398@gmail.com>
2020-07-15 08:18:26 -0300

    implementando rotas (react router)

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,18 +1,13 @@
 import "./Nav.css";
 import React from "react";
+
 import NavItem from "./NavItem";
 
 export default (props) => (
   <aside className="menu-area">
     <nav className="menu">
-      {/* <a href="#/">
-        <i className="fa fa-home"></i> Início
-      </a>
-      <a href="#/users">
-        <i className="fa fa-users"></i> Usuários
-      </a> */}
-      <NavItem path="#/" icon="home" title="Início" />
-      <NavItem path="#/users" icon="users" title="Usuários" />
+      <NavItem path="/" icon="home" title="Início" />
+      <NavItem path="/users" icon="users" title="Usuários" />
     </nav>
   </aside>
 );
--- frontend/src/components/template/NavItem.jsx
+++ frontend/src/components/template/NavItem.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default (props) => (
-  <a href={props.path}>
+  <Link to={props.path}>
     <i className={`fa fa-${props.icon}`}></i> {props.title}
-  </a>
+  </Link>
 );
