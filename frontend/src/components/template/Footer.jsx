624b058355694b83ae606a994a9f90c431df26b1
Lucas Sales <lucassales398@gmail.com>
2020-07-15 07:13:01 -0300

    componente footer

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -1,4 +1,8 @@
 import "./Footer.css";
 import React from "react";
 
-export default (props) => <footer className="footer">Footer</footer>;
+export default (props) => (
+  <footer className="footer">
+    <span>Alguns direitos reservados.</span>
+  </footer>
+);
